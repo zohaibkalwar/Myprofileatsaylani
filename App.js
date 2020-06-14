1009a16c145b02ef3a744848b8b27fa27545c957
zohaibkalwar <zohaib.kalwar@gmail.com>
2020-06-14 19:28:17 +0500

    JavaScript Assignements

---
+++ App.js
@@ -0,0 +1,188 @@
+// ///////// Chapter 1 * Task # 1 \\\\\\\\\\\\\
+// alert("Welcome into My JavaScript Assignements");
+
+// ///////// Chapter 1 * Task # 2 \\\\\\\\\\\\\
+// alert("Please Enter a Valid Password.");
+
+// ///////// Chapter 1 * Task # 3 \\\\\\\\\\\\\
+// alert("Wellcome to JS Land...\n Happy Coding!");
+
+// ///////// Chapter 1 * Task # 4 \\\\\\\\\\\\\
+// alert("Wellcome to JS Land...");
+
+// ///////// Chapter 1 * Task # 4 \\\\\\\\\\\\\
+// alert("Happy Coding!");
+
+// ///////// Chapter 1 * Task # 5 \\\\\\\\\\\\\
+// alert("Hello... I Can run JS through my web brower's console")
+
+// ///////// Chapter 1 * Task # 6 \\\\\\\\\\\\\
+
+
+// ///////// Chapter 1 * Task # 7 \\\\\\\\\\\\\
+// {/* <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>JAvaScript Assignments</title>
+// </head>
+// <body>
+//     <H1>JAvaScript Assignments</H1>
+//     <H2>Chapter 1 to 3</H2>
+// </body>
+// <script src="App.js"></script>
+// </html> */}
+
+// ///////// Chapter 2 ** Task # 1 \\\\\\\\\\\\\
+// var username = 'Zohaib';
+
+// ///////// Chapter 2 ** Task # 2 \\\\\\\\\\\\\
+// var myname = 'Zohaib Hassan';
+
+// ///////// Chapter 2 ** Task # 3 \\\\\\\\\\\\\
+// var message = "Hello World";
+// alert(message);
+
+// ///////// Chapter 2 ** Task # 4 \\\\\\\\\\\\\
+// var name = prompt("Enter Your Name");
+// var age = prompt("Enter Your Age");
+// var text = alert("Certified Mobile Application Development");
+
+///////// Chapter 2 ** Task # 5 \\\\\\\\\\\\\
+// var PIZZA = "PIZZA \nPIZZ \nPIZ \nPI \nP "; 
+// alert(PIZZA);
+
+///////// Chapter 2 ** Task # 6 \\\\\\\\\\\\\
+//  var email = alert('My email address is zohaib.kalwar@gmail.com');
+
+///////// Chapter 2 ** Task # 7 \\\\\\\\\\\\\
+// var Book = alert('I am trying to learn from the Book a Smarter way to learn JavaScript');
+
+///////// Chapter 2 ** Task # 8 \\\\\\\\\\\\\
+// document.write('Yah! I can write HTML content through JavaScript');
+
+///////// Chapter 2 ** Task # 9 \\\\\\\\\\\\\
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+///////// Chapter 3 *** Task # 1 \\\\\\\\\\\\\
+// var age = "I am 33 Years Old"
+// alert(age);
+
+///////// Chapter 3 *** Task # 2 \\\\\\\\\\\\\
+
+
+///////// Chapter 3 *** Task # 3 \\\\\\\\\\\\\
+// var birthyear = "My birth year is 1987";
+// document.write(birthyear);
+
+///////// Chapter 3 *** Task # 4 \\\\\\\\\\\\\
+// var visitor = prompt("Enter Visitor Name");
+// var Product = prompt("Enter Product Name");
+// var quantity = prompt("Enter QTY");
+// document.write("<b>", visitor, "</b>", " Ordered " , "<b>", quantity, " ", Product,"</b>", " on www.rajpootlunchbox.com");
+
+
+///////// Chapter 4 **** Task # 1 \\\\\\\\\\\\\
+// var name = "Zohaib Hassan", rollnumber = "WM12592", Cellphone = "03158111741";
+// document.write(name, rollnumber, " ", Cellphone);
+
+///////// Chapter 4 **** Task # 2 \\\\\\\\\\\\\
+// var Legal1 = "Zohaib";
+// var legal2 = "Hassan";
+// var legal3 = "Kalwar";
+// var legal4 = "WM12592";
+// var legal5 = "03158111741";
+
+// var 1Legal = "Zohaib";
+// var -legal = "Hassan";
+// var #legal = "Kalwar";
+// var @legal = "WM12592";
+// var *legal = "03158111741";
+
+///////// Chapter 4 **** Task # 3 \\\\\\\\\\\\\
+// document.write("<H1> Rules for naming JS variables </H1>");
+
+
+///////// Chapter 5 **** Task # 1 = + \\\\\\\\\\\\\
+// var val1 = prompt("Enter your Fist Value");
+// var operator = prompt("Enter your operator");
+// var val2 = prompt("Enter your 2nd Value");
+// if(operator === '+'){
+//     alert((+val1)+(+val2))
+// }else "Non";
+// document.write("Sum of ", val1, "and ", val2, " = ", (+val1)+(+val2));
+
+///////// Chapter 5 **** Task # 2 = - \\\\\\\\\\\\\
+// var val1 = prompt("Enter your Fist Value");
+// var operator = prompt("Enter your operator");
+// var val2 = prompt("Enter your 2nd Value");
+// if(operator === '-'){
+//     alert((+val1)-(+val2))
+// }else "Non";
+// document.write("subtraction of ", val1, "and ", val2, " = ", (+val1)-(+val2));
+
+///////// Chapter 5 **** Task # 3 = * \\\\\\\\\\\\\
+// var val1 = prompt("Enter your Fist Value");
+// var operator = prompt("Enter your operator");
+// var val2 = prompt("Enter your 2nd Value");
+// if(operator === '*'){
+//     alert((+val1)*(+val2))
+// }else "Non";
+// document.write("Multipication of ", val1, "and ", val2, " = ", (+val1)*(+val2));
+
+///////// Chapter 5 **** Task # 3 = / \\\\\\\\\\\\\
+// var val1 = prompt("Enter your Fist Value");
+// var operator = prompt("Enter your operator");
+// var val2 = prompt("Enter your 2nd Value");
+// if(operator === '/'){
+//     alert((+val1)/(+val2))
+// }else "Non";
+// document.write("Multipication of ", val1, "and ", val2, " = ", (+val1)/(+val2));
+
+///////// Chapter 5 **** Task # 3 \\\\\\\\\\\\\
+// var variable = " Value after variable declaration is: 5";
+// document.write(variable);
+// var initial = "<br> Initital value is : 5";
+// document.write(initial);
+
+
+
+
+
+///////// Chapter 5 **** Task # 4 \\\\\\\\\\\\\
+// var ticket = 600;
+// var movie = prompt("Enter Your Quantity of Tickets");
+// document.write("Total cost to buy ", movie, " Tickets to a movie is ",  movie * ticket, "PKR");
+
+///////// Chapter 5 **** Task # 5 \\\\\\\\\\\\\
+// document.write("<b> Table of 4 </b>", "<br>")
+// document.write(" 4 " + " X " + " 1 " + " = " + 4 * 1 + "<br>")
+// document.write(" 4 " + " X " + " 1 " + " = " + 4 * 2 + "<br>")
+// document.write(" 4 " + " X " + " 1 " + " = " + 4 * 3 + "<br>")
+// document.write(" 4 " + " X " + " 1 " + " = " + 4 * 4 + "<br>")
+// document.write(" 4 " + " X " + " 1 " + " = " + 4 * 5 + "<br>")
+// document.write(" 4 " + " X " + " 1 " + " = " + 4 * 6 + "<br>")
+// document.write(" 4 " + " X " + " 1 " + " = " + 4 * 7 + "<br>")
+// document.write(" 4 " + " X " + " 1 " + " = " + 4 * 8 + "<br>")
+// document.write(" 4 " + " X " + " 1 " + " = " + 4 * 9 + "<br>")
+// document.write(" 4 " + " X " + " 1 " + " = " + 4 * 10 + "<br>")
+
+///////// Chapter 5 **** Task # 7 \\\\\\\\\\\\\
+// var product1 = "Apple"
+// var PPrice1 = "50"
+// var qty1 = prompt("Enter QTY for ", product1 + PPrice1, "Per KG" )
+// document.write("<H1> Shopping Cart </H1> <br>", "Price of ", product1, " Is ", PPrice1, " Per KG and Total Amount is ", qty1 * PPrice1);
+// document.write("<Br> Quantity of ", product1, " is ", qty1);
+// var product2 = "Mango"
+// var PPrice2 = "90"
+// var qty2 = prompt("Enter QTY for ", product2 + PPrice2, "Per KG") 
+// document.write("<br> Price of ", product2, " Is ", PPrice2, " Per KG and Total Amount is ", qty2 * PPrice2)
+// document.write("<Br> Quantity of ", product2, " is ", qty2);
+// var shipping = 100;
+// document.write("<br> Shipping Charges Rs.", shipping, "<br>");
+// document.write("<br> Total Cost of Your Order is ", (qty1*PPrice1)+(qty2*PPrice2)+shipping);
+
+
+///////// Chapter 5 **** Task # 8 \\\\\\\\\\\\\
+// var markstotal = +prompt("Enter your Total Marks");
+// var marksobtain = +prompt("Enter Your Obtained Marks");
+// document.write("Total Marks: ", markstotal, "<br> Marks Obtained: ", marksobtain, "<br> Percentage: ", marksobtain/markstotal,"%");
