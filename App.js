8ccfe4515e8d15b2a45554dc04ac3d0179e2316e
zohaibkalwar <zohaib.kalwar@gmail.com>
2020-06-20 16:01:16 +0500

    Assignment # 5

--- App.js
+++ App.js
@@ -185,4 +185,299 @@
 ///////// Chapter 5 **** Task # 8 \\\\\\\\\\\\\
 // var markstotal = +prompt("Enter your Total Marks");
 // var marksobtain = +prompt("Enter Your Obtained Marks");
-// document.write("Total Marks: ", markstotal, "<br> Marks Obtained: ", marksobtain, "<br> Percentage: ", marksobtain/markstotal,"%");
+// var Percentage = marksobtain / markstotal;
+// document.write("Total Marks: ", markstotal, "<br> Marks Obtained: ", marksobtain, "<br> Percentage: ", Percentage.toFixed(2),"%" );
+
+// ///////// Chapter 5 **** Task # 9 \\\\\\\\\\\\\
+// var doller = +prompt("Enter Doller to PKR Convertion", "Number of Dollers") * 104.80;
+// var saudi = +prompt("Enter Saudi Riyal to PKR Convertion", "Number of Riyals") * 44;
+// document.write("Doller Convertion with PKR ", doller, "<br> </br>");
+// document.write("Saudi Riyal Convertion with PKR ", saudi);
+
+// ///////// Chapter 5 **** Task # 10 \\\\\\\\\\\\\
+// var numbers = 5 * 10 / 2;
+// document.write(numbers);
+
+// ///////// Chapter 5 **** Task # 11 \\\\\\\\\\\\\
+// var CurrentYear = 2020;
+// var birthyear = 1987;
+// var Yourage = CurrentYear - birthyear;
+// document.write("Current Year: ", CurrentYear, "<br> Birht Year: ", birthyear, "<br> Your Age is: ", Yourage);
+
+// ///////// Chapter 5 **** Task # 12 \\\\\\\\\\\\\
+// Too hard to complete the Task
+
+// ///////// Chapter 5 **** Task # 13 \\\\\\\\\\\\\
+// var snaks = "lays";
+// var agge = 15;
+// var maxage = 65;
+// var perday = 3;
+// var Remainage = (maxage - agge) * (perday);
+// document.write("Favourite Snaks: ", snaks, "<br> Current Age: ", agge, "<br> Estimated Maximum Age: ", maxage, "<br> Amount of Snaks per day: ", Remainage);
+
+
+// ///////// Chapter 6-9 ***** Task # 1 \\\\\\\\\\\\\
+// var x = +prompt("Enter Your Value");
+// document.write("The Value of X is: ", x ,"<br> ............................ ", "<br>");
+// ++x;
+// var z = x;
+// document.write("<br> The value of ++x is: ", z, "<br> Now the Value of X is: ", z, "<br>");
+// var x11 = x;
+// x11++;
+// var xy = x11;
+// document.write("<br> The value of ++x is: ", xy, "<br> Now the Value of X is: ", xy , "<br>");
+// var x12 = x11;
+// --x12;
+// var xz = x12;
+// document.write("<br> The value of --x is: ", xz, "<br> Now the Value of X is: ", xz , "<br>");
+// var x13 = x12;
+// x13--;
+// var xzz = x13;
+// document.write("<br> The value of x-- is: ", xz, "<br> Now the Value of X is: ", xzz);
+
+
+// ///////// Chapter 6-9 ***** Task # 2 \\\\\\\\\\\\\
+// var a = 2, b = 1; 
+// var result = --a - --b + ++b + b--;
+// document.write("A value is = ", a, "<br> B value is = ", b, "<br> Result is = ", result);
+
+// ///////// Chapter 6-9 ***** Task # 3 \\\\\\\\\\\\\
+// var usergreet = prompt("Please Enter Your UserName ","Zohaib.Hassan");
+// var greet = alert("Welcome to JavaScript World!");
+
+// ///////// Chapter 6-9 ***** Task # 5 \\\\\\\\\\\\\
+
+// var table = +prompt("Enter Your Table Number ");
+// var figure = table;
+// for(var table = 1; table <= 10; table++){
+//     document.write(figure + " X " + table + " = " + table * figure, "<br>");
+//}
+
+// ///////// Chapter 6-9 ***** Task # 6 \\\\\\\\\\\\\
+// var subject1 = prompt("Enter Subject 1 ");
+// var subject2 = prompt("Enter Subject 2 ");
+// var subject3 = prompt("Enter Subject 3 ");
+// var totalmarks = 100;
+// var obtained1 = +prompt("Enter Your Obtained Marks of Subject 1 ");
+// var obtained2 = +prompt("Enter Your Obtained Marks of Subject 2 ");
+// var obtained3 = +prompt("Enter Your Obtained Marks of Subject 3 ");
+// document.write("<br> Subject Total Marks Obtained Marks Percentage <br>");
+// document.write(subject1, " ", totalmarks ," ", obtained1, " ", obtained1/totalmarks, "% <br>");
+// document.write(subject2, " ", totalmarks ," ", obtained2, " ", obtained2/totalmarks, "% <br>");
+// document.write(subject3, " ", totalmarks ," ", obtained3, " ", obtained3/totalmarks, "% <br>");
+// document.write(totalmarks*3, " ", obtained3+obtained1+obtained2, " ",)
+
+
+// ///////// Chapter 9-11 ***** Task # 1 \\\\\\\\\\\\\
+// var city = prompt('Enter City Name');
+// var arr = ['Karachi','Sukkur']
+// for (var i=0; i<= arr.length; i++){
+//     if(arr[i] === city){
+//         alert('Welcome to City of Lights')
+//         break;
+//     }else{alert('City Not Found')}
+// }
+
+
+// ///////// Chapter 9-11 ***** Task # 2 \\\\\\\\\\\\\
+// var gender = prompt("Enter Gender Plase" ,"Male or Female");
+// switch(gender){
+//     case "Male":
+//         alert("Good Morning Sir")
+//         break
+//     case "Female":
+//         alert("Good Morning Maam")
+//         break
+//     default:
+//         alert("Good Morning")
+// }
+
+// ///////// Chapter 9-11 ***** Task # 4 \\\\\\\\\\\\\
+// var car = +prompt("Enter the remaining Fuel in car ", "in litters");
+// var msg = 0.25;
+// if(car <= msg){
+//     alert("Refill your car fuel ")
+// }else{alert("Enjoy your Ride")}
+
+// ///////// Chapter 9-11 ***** Task # 5 \\\\\\\\\\\\\
+// var a = 4; 
+// if (++a === 5){ 
+//     alert("given condition for variable a is true"); 
+// }   
+// var b = 82; 
+// if (b++ - 83){ 
+//     alert("given condition for variable b is true"); 
+// } 
+// var c = 12; 
+// if (++c === 13){ 
+//     alert("condition 1 is true"); 
+// } 
+// if (c === 13){ 
+//     alert("condition 2 is true"); 
+// } 
+// if (++c <= 14){ 
+//     alert("condition 3 is true"); 
+// } 
+// if(c === 14){ 
+//     alert("condition 4 is true"); 
+// } 
+ 
+// var materialCost = 20000; 
+// var laborCost = 2000; 
+// var totalCost = materialCost + laborCost; 
+// if (totalCost === laborCost + materialCost){ 
+//     alert("The cost equals"); 
+// } 
+// if (true){ 
+//     alert("True"); 
+// } if (false){ 
+//     alert("False"); 
+// } 
+// if("car" < "cat"){ 
+//     alert("car is smaller than cat"); 
+// } 
+ 
+
+/////////// Chapter 9-11 ***** Task # 6 \\\\\\\\\\\\\
+// var sub1 = prompt("Enter Subject 1 ");
+// var sub2 = prompt("Enter Subject 2 ");
+// var sub3 = prompt("Enter Subject 3 ");
+// var obt1 = +prompt("Enter Subject 1 Obtain Marks ");
+// var obt2 = +prompt("Enter Subject 2 Obtain Marks ");
+// var obt3 = +prompt("Enter Subject 3 Obtain Marks ");
+// var grade = ['A-One','A','B','Fail'];
+// var rmks = ['Excelent', 'Good' , 'You Need to Improve ','Sorry'];
+// var totalmkr = 100*3;
+// var obt = obt1+obt2+obt3;
+// var pctg = obt/totalmkr;
+// document.write("Total Marks : ",totalmkr);
+// document.write("<br> Marks Obtained : ", obt);
+// document.write("<br> Percentage : ", pctg.toFixed(2), "%");
+// if(pctg >= 0.80){
+//     document.write("<br> Grade ", "<b>", grade[0] ,"</b>")
+// }else if(pctg >= 0.70){
+//     document.write("<br> Grade ","<b>",grade[1] ,"</b>")
+// }else if(pctg >= 0.60){
+//     document.write("<br> Grade ","<b>",grade[2] ,"</b>")
+// }else if(pctg < 0.60){
+//     document.write("<br> Grade ","<b>",grade[3] ,"</b>")
+// }else{alert("Not Working")};
+// if(pctg >= 0.80){
+//     document.write("<Br> Remarks ", "<b> ", rmks[0])
+// }else if(pctg >= 0.70){
+//     document.write("<Br> Remarks ","<b> ", rmks[1])
+// }else if(pctg >= 0.60){
+//     document.write("<br> Remarks ", "<b>", rmks[2])
+// }else if(pctg < 0.60){
+//     document.write("<br> Remarks ", "<b>", rmks[3])
+// }else{document.write("Error on Remarks veriable",rmks) }
+
+
+/////////// Chapter 9-11 ***** Task # 7 \\\\\\\\\\\\\
+
+
+/////////// Chapter 9-11 ***** Task # 8 \\\\\\\\\\\\\
+
+
+/////////// Chapter 9-11 ***** Task # 9 \\\\\\\\\\\\\
+// var num = +prompt("Enter number")
+// if ( num % 2 == 0) {
+// 	alert('Even Number');
+// }else{
+// 	alert('Odd Number');
+// }
+
+/////////// Chapter 9-11 ***** Task # 10 \\\\\\\\\\\\\
+// var tempr = +prompt("Enter Temprature values", "40")
+// var temp = ['It is too hot outside','The Weather today is Normal','Today’s Weather is cool','OMG! Today’s weather is so Cool'];
+// if(tempr >= 40){
+//     alert(temp[0])
+// }else if(tempr >= 30){
+//     alert(temp[1])
+// }else if(tempr >= 20){
+//     alert(temp[2])
+// }else if(tempr <= 10){
+//     alert(tempr[3])
+// }else ("Errors")
+
+
+/////////// Chapter 9-11 ***** Task # 11 \\\\\\\\\\\\\
+// var num1 = +prompt("Enter Value 1 ");
+// var Opter = prompt("Operator Sign","+");
+// var num2 = +prompt("Enter Value 2 ");
+// if(Opter === "+"){
+//     document.write(num1+num2)
+// }else if(Opter === "-"){
+//     document.write(num2-num2)
+// }else if (Opter === "/"){
+//     document.write(num1/num2)
+// }else if(Opter === "*"){
+//     document.write(num1*num2)
+// }else if(Opter === "%"){
+//     document.write(num1%num2)
+// }else("Check Your Operator")
+
+/////////// Chapter 12-13 ***** Task # 1 \\\\\\\\\\\\\
+// var word = prompt("Enter your Word!");
+// var code = word.charCodeAt(0);
+// document.write(code);
+
+/////////// Chapter 12-13 ***** Task # 2 \\\\\\\\\\\\\
+
+
+
+
+/////////// Chapter 12-13 ***** Task # 3 \\\\\\\\\\\\\
+
+
+/////////// Chapter 12-13 ***** Task # 4 \\\\\\\\\\\\\
+
+/////////// Chapter 12-13 ***** Task # 5 \\\\\\\\\\\\\
+// var enterpass = prompt("Enter your Password");
+// var storepass = ['Password','Hello'];
+// if(enterpass === storepass[0] || enterpass === storepass[1] ){
+//     alert("You have already Entered Old Please Type New ");
+// }else ("Password Has been Changed")
+
+
+/////////// Chapter 12-13 ***** Task # 6 \\\\\\\\\\\\\
+// var greeting; 
+// var hour = 13; 
+// if (hour < 18) { 
+//     greeting = alert("Good day"); 
+// }else greeting = alert("Good evening");
+ 
+ 
+/////////// Chapter 12-13 ***** Task # 7 \\\\\\\\\\\\\
+//  var timeinput = +prompt("Enter Time in 24 hours format");
+//  if(timeinput >= 0000 && 1200 ){
+//      alert("Good Morning");
+//  }else if(timeinput >= 1200 && 1700 ){
+//      alert("Good After Noon")
+//  }else if(timeinput >= 1700){
+//      alert("Good Evening ")
+//  }else if(timeinput <=2359){
+//      alert("Good Night")
+//  }else ("Please Enter Correct Time")
+
+
+/////////// Chapter 14-16 ***** Task # 1 \\\\\\\\\\\\\
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
